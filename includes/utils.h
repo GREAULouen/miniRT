34e2edd8ef418f9178bdbfc6a740c2feb6ba998b
Louen Greau <lgreau@1-A-16.42heilbronn.de>
2024-05-24 12:55:51 +0200

    Added C1 + k*C2 color_utils calculation

--- includes/utils.h
+++ includes/utils.h
@@ -6,7 +6,7 @@
 /*   By: lgreau <lgreau@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/14 16:47:35 by lgreau            #+#    #+#             */
-/*   Updated: 2024/05/24 12:05:00 by lgreau           ###   ########.fr       */
+/*   Updated: 2024/05/24 12:53:52 by lgreau           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,5 +36,6 @@ uint32_t		get_blue(uint32_t color);
 uint32_t		color_k_mult(uint32_t color, int k);
 uint32_t		color_scal_mult(uint32_t color, double k);
 uint32_t		color_add(uint32_t color1, uint32_t color2);
+uint32_t		color_add_scal(uint32_t color1, double k, uint32_t color2);
 
 #endif
